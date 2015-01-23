23b9f66926a0eb6a20cd443832a79866775b847f
Andrea Catalini <andrea.catalini89@gmail.com>
2015-01-23 22:30:09 +0100

    Shuffle method works!!

--- Assets/Scripts/ShuffleBehaviour/Manager.cs
+++ Assets/Scripts/ShuffleBehaviour/Manager.cs
@@ -4,21 +4,29 @@ using System.Collections.Generic;
 
 public class Manager : MonoBehaviour {
 
-   
-    List<GameObject> chunks1;
+    public GameObject chunkParent;
+    //public Transform debugMin, debugMax;
+    public float speedLearp;
+    private GameObject[] chunks1;
     private float screenWidth;
     private float screenHeight;
 	// Use this for initialization
     void Awake()
     {
         Vector2 topRightCorner = new Vector2(1, 1); //viewport space places (1,1) at the top-right corner
-        Vector2 edgeVector = camera.ViewportToWorldPoint(topRightCorner);
-        screenHeight = edgeVector.y * 2;
-        screenWidth = edgeVector.x * 2;
-        chunks1 = new List<GameObject>();
+        Vector2 edgeVector = Camera.main.ViewportToWorldPoint(topRightCorner);
+        screenHeight = edgeVector.y;
+        Debug.Log("top right corner in world coordinate = " + edgeVector);
+        screenWidth = edgeVector.x;
+       // debugMin.transform.position = new Vector3(-screenWidth, -screenHeight);
+      //  debugMax.transform.position = new Vector3(screenWidth, screenHeight);
     }
 	void Start () {
-	    
+        chunks1 = new GameObject[chunkParent.transform.childCount];
+	    for(int i =0 ; i<chunkParent.transform.childCount; i++)
+        {
+            chunks1[i] = chunkParent.transform.GetChild(i).gameObject;
+        }
 	}
 	
 	// Update is called once per frame
@@ -34,7 +42,11 @@ public class Manager : MonoBehaviour {
         {
             x = Random.Range(-screenWidth, screenWidth);
             y = Random.Range(-screenHeight, screenHeight);
+            Debug.Log("x = " + x + " y = " + y);
+            iTween.MoveTo(chunk, new Vector2(x, y), speedLearp);
+            //chunk.transform.position = new Vector2(x, y);
         }
+        
         
     }
 }
