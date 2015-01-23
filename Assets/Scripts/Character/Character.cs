3c7fd4d52ce269a3d38fc31b982583703934f5f6
mihai9323 <mihai9323@gmail.com>
2015-01-23 22:30:06 +0100

    Movement feature

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -3,13 +3,43 @@ using System.Collections;
 
 public class Character : MonoBehaviour {
 
+    public Transform minPosition, maxPosition;
+    private Vector2 min{
+     get{
+        return minPosition.transform.position;
+     }
+    }
+    private Vector2 max
+    {
+        get
+        {
+            return maxPosition.transform.position;
+        }
+    }
+    public int score;
 	// Use this for initialization
 	void Start () {
 	
 	}
+    void FixedUpdate()
+    {
+        if (Died())
+        {
+            ReSpawn();
+        }
+    }
+
+    private bool Died()
+    {
+        return transform.position.x < min.x || transform.position.y < min.y;
+    }
+
+    void ReSpawn()
+    {
+        transform.position = new Vector3(Random.value * max.x, Random.value * max.y, 0);
+        score -= 10;
+
+    }
 	
-	// Update is called once per frame
-	void Update () {
 	
-	}
 }
---
+++ Assets/Scripts/World/WorldMover.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldMover : MonoBehaviour {
+    
+    public float speed;
+    
+  
+    void FixedUpdate()
+    {
+
+        this.transform.position += new Vector3(speed,0,0) * Time.fixedDeltaTime;
+    }
+}
